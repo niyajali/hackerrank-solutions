1f8893130fbd2b00da05d15cbaf9be316d7c1313
Sk Niyaj Ali <niyaj639@gmail.com>
2023-05-21 19:38:18 +0530

    DS[LinkedList][4] - ReverseList

---
+++ src/main/kotlin/data_structure/linked_list/ReverseList.kt
@@ -0,0 +1,48 @@
+package data_structure.linked_list
+
+import java.util.*
+
+/*
+ * Complete the 'reversePrint' function below.
+ *
+ * The function accepts INTEGER_SINGLY_LINKED_LIST list as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data: Int
+ *     next: SinglyLinkedListNode
+ * }
+ *
+ */
+
+fun reversePrint(list: SinglyLinkedListNode?) {
+    // Write your code here
+    if (list != null) {
+        if(list.next != null) {
+            reversePrint(list.next)
+        }
+
+        println(list.data)
+    }
+}
+
+fun main() {
+    val scan = Scanner(System.`in`)
+
+    val tests = scan.nextLine().trim().toInt()
+
+    for (testsItr in 1..tests) {
+        val count = scan.nextLine().trim().toInt()
+        val list = SinglyLinkedList()
+
+        for (i in 0 until count) {
+            val listItem = scan.nextLine().trim().toInt()
+            list.insertNode(listItem)
+        }
+
+        reversePrint(list.head)
+    }
+}
